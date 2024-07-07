f04d994c234aa9cd402c02131ff190d81ca98e2e
elieljohn <ejmalabar88@gmail.com>
2024-07-07 16:55:12 +0100

    Logging Exceptions
    1. Add a try-catch block on the GetPointsOfInterest() function so that it logs exceptions on the console and returns an error message on the catch block.
    PointsOfInterestController.cs: lin 22-42

--- CityInfo/CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ CityInfo/CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3dfb8f024a8627214b799ae81f08cc3a095132c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9090239094b618089f6ae7da2bd2a752a49119a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
