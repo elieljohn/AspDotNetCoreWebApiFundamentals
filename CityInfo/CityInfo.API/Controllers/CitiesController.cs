6e247b1c8f05cdeb5a2ad6bf8cedb25b73dcd564
elieljohn <ejmalabar88@gmail.com>
2024-07-20 16:35:10 +0100

    Supporting Versioning
    1. Install Asp.Versioning.Mvc via NuGet packages
    
    2. Call AddApiVersioning service and chain AddMvc to configure API versioning and add MVC services.
    - ReportApiVersions enables the reporting of API versions in response headers.
    - AssumeDevaultVersionWhenUnspecified assumes the default API version if the client does not specify a version.
    - DefaultApiVersion sets the default API version to 1.0.
    Program.cs: line 77-82

--- CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -9,7 +9,7 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace CityInfo.API.Controllers
 {
     [ApiController]
-    [Authorize]
+    //[Authorize]
     [Route("api/cities")]
     public class CitiesController : ControllerBase
     {
